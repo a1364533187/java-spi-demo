8b7d008c9fd3a633ac169842c8fbbeb53ab541d0
suzhiwu <suzhiwu@kuaishou.com>
2022-04-14 21:22:36 +0800

    add Code1

--- spi-biz/src/main/java/com/bigcow/com/spi/biz/code/mycode/huawei/Code1.java
+++ spi-biz/src/main/java/com/bigcow/com/spi/biz/code/mycode/huawei/Code1.java
@@ -2,7 +2,6 @@ package com.bigcow.com.spi.biz.code.mycode.huawei;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Code1 {
@@ -11,8 +10,8 @@ public class Code1 {
         List<Integer> list = new ArrayList<>();
         list.add(0, 3);
         System.out.println(list);
-//        int[][] inputs = { { 7, 0 }, { 4, 4 }, { 7, 1 }, { 5, 0 }, { 6, 1 }, { 5, 2 } };
-//        List<int[]> res = reOrder(inputs);
+        //        int[][] inputs = { { 7, 0 }, { 4, 4 }, { 7, 1 }, { 5, 0 }, { 6, 1 }, { 5, 2 } };
+        //        List<int[]> res = reOrder(inputs);
 
     }
 
@@ -51,25 +50,6 @@ public class Code1 {
         for (int i = 0; i < inputs.length; i++) {
             int[] input = inputs[i];
             res.add(input[1], input);
-        }
-        return res;
-    }
-
-    public int[][] reconstructQueue(int[][] people) {
-        // 对inputs 排序
-        Arrays.sort(people, (o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            }
-            return o2[0] - o1[0];
-        });
-        List<int[]> resList = new LinkedList<>();
-        for (int i = 0; i < people.length; i++) {
-            resList.add(people[i][1], people[i]);
-        }
-        int[][] res = new int[people.length][2];
-        for (int i = 0; i < resList.size(); i++) {
-            res[i] = resList.get(i);
         }
         return res;
     }
